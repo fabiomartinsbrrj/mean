ff7da5bcc73d53d768da1ee15d829c865ecdaefd
fabiomartinsbr <fabiomartinsbr@gmail.com>
2017-05-19 22:52:08 -0300

    Services for CRUD created

---
+++ backend/config/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+module.exports = function (server) {
+    //API Routes
+
+    const router = express.Router();
+
+    server.use('/api', router);//context http://localhost:3003/api/
+
+    router.route('/teste').get(function (req, res, next) {
+       res.send('funcionou'); 
+    });
+
+    //routes of API
+    const billingCycleService = require('../api/billingCycle/billingCycleService');
+    billingCycleService.register(router, '/billingCycles');
+};
