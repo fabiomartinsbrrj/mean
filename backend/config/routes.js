b04a101e95d38ae2e9cac95ad6ae8bcba40d5710
fabiomartinsbr <fabiomartinsbr@gmail.com>
2017-06-15 13:38:35 -0300

    backend with authentication

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,20 +1,35 @@
 const express = require('express');
+const auth = require('./auth');
 
 module.exports = function (server) {
-    //API Routes
+    /*
+    *   rotas abertas
+    */
+    const openAPI = express.Router();
+    server.use('/oapi', openAPI);
 
-    const router = express.Router();
-    server.use('/api', router);//context http://localhost:3003/api/
-
-    router.route('/teste').get(function (req, res, next) {
+    const AuthService = require('../api/user/authService')
+    openAPI.post('/login', AuthService.login);
+    openAPI.post('/signup', AuthService.signup);
+    openAPI.post('/validateToken', AuthService.validateToken);
+    
+    openAPI.route('/teste').get(function (req, res, next) {
        res.send('funcionou'); 
     });
 
+    /**
+     * Rotas protegidas
+     */
+    const protectedAPI = express.Router();
+    server.use('/api', protectedAPI);//context http://localhost:3003/api/
+
+    protectedAPI.use(auth);
+
     //routes of API
     const billingCycleService = require('../api/billingCycle/billingCycleService');
-    billingCycleService.register(router, '/billingCycles');
+    billingCycleService.register(protectedAPI, '/billingCycles');//usando a api do node-restful
 
     const billingSummaryService = require('../api/billingSummary/billingSummaryService')
-    router.route('/billingSummary').get(billingSummaryService.getSummary)
+    protectedAPI.route('/billingSummary').get(billingSummaryService.getSummary);//usando a api do express
 
 };
